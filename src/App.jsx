b2d597d88c659a15bdbebca5ce84af3901a95059
Mario Niko <marios-2222@hotmail.com>
2024-02-25 11:49:03 +0200

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ import Home from "./pages/Home"
 import SignIn from "./pages/Signin"
 import SignUp from "./pages/SignUp"
 import About from "./pages/About"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+
 function App() {
 
   return (
   <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/sign-in" element={<SignIn/>} />
@@ -14,6 +18,7 @@ function App() {
       <Route path="/about" element={<About/>} />
 
     </Routes>
+    <Footer />
   </BrowserRouter>
   )
 }
